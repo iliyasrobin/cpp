d8603296ab7020ec740f3be0305809bbe2afa217
Iliyas Robin <73294126+iliyasrobin@users.noreply.github.com>
2021-10-01 13:51:02 +0600

    Add files via upload

---
+++ 3 by 3 stars.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    cout<<"Hello Good People's"<<endl;
+    cout<<" 3*3 Stars Printing Using C++"<<endl;
+    for(int i=1;i<=3;i++){
+        for (int j=1;j<=3;j++){
+            cout<<" * ";
+        } cout<<endl;
+    }cout<<endl;
+}
